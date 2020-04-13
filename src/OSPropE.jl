48aee941f0b0ea734aee82841c0d4c02f6471408
Kelvyn Baruc Sánchez Sánchez <kelvyn.baruc@gmail.com>
2020-04-13 15:10:24 -0500

    fix in main module

--- src/OSPropE.jl
+++ src/OSPropE.jl
@@ -2,18 +2,6 @@ module OSPropE
   # Function to call the GUI
   export OSPropEGUI
 
-  using Gtk, Gtk.ShortNames, JLD, Suppressor, CSV, Mustache, Dates
-  import DataFrames
-
-  # Path to CSS Gtk-Style dataFile
-  global style_file = joinpath(dirname(Base.source_path()), "style2020.css")
-
-  # General Settings
-  global pathPUREDIPPR = joinpath(dirname(Base.source_path()), "PUREDIPPR.csv")
-
-  # Load default database
-  global databaseDIPPR = CSV.read(pathPUREDIPPR)
-
   # Include the main file .fl
   include("mainGUI_OSPropE.jl")
 end
