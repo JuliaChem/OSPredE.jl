c137622582b7f4f33f7d7441208c11ec361cdc39
Kelvyn Baruc Sánchez Sánchez <kelvyn.baruc@gmail.com>
2020-04-13 16:31:06 -0500

    allow access to path in linux

--- src/OSPropE.jl
+++ src/OSPropE.jl
@@ -2,6 +2,11 @@ module OSPropE
   # Function to call the GUI
   export OSPropEGUI
 
+  if Sys.islinux()
+    pkgpath = joinpath(dirname(Base.source_path()))
+    ;chmod +rwxc $pkgpath
+  end
+
   # Include the main file .fl
   include("mainGUI_OSPropE.jl")
 end
