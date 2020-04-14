e11123a016fc811bdb2676b12263075efc2d83be
Kelvyn Baruc Sánchez Sánchez <kelvyn.baruc@gmail.com>
2020-04-13 23:44:21 -0500

    Basic structure added

--- src/mainGUI_OSPropE.jl
+++ src/mainGUI_OSPropE.jl
@@ -17,10 +17,13 @@ global style_file = joinpath(dirname(Base.source_path()), "style2020.css")
 # General Settings
 # Database path
 if Sys.iswindows()
-    global pathPUREDIPPR = joinpath(dirname(Base.source_path()), "database\\PUREDIPPR.csv")
-    global filename_in = joinpath(dirname(Base.source_path()), "img\\molsvg.svg")
-    global filename_out = joinpath(dirname(Base.source_path()), "img\\molpng.png")
-    imgpath = joinpath(dirname(Base.source_path()),"img")
+    global pathPUREDIPPR =
+        joinpath(dirname(Base.source_path()), "database\\PUREDIPPR.csv")
+    global filename_in =
+        joinpath(dirname(Base.source_path()), "img\\molsvg.svg")
+    global filename_out =
+        joinpath(dirname(Base.source_path()), "img\\molpng.png")
+    imgpath = joinpath(dirname(Base.source_path()), "img")
 
     try
         mkdir(imgpath)
@@ -30,12 +33,14 @@ if Sys.iswindows()
 end
 
 if Sys.islinux()
-    global pathPUREDIPPR = joinpath(dirname(Base.source_path()), "database/PUREDIPPR.csv")
+    global pathPUREDIPPR =
+        joinpath(dirname(Base.source_path()), "database/PUREDIPPR.csv")
     global filename_in = joinpath(dirname(Base.source_path()), "img/molsvg.svg")
-    global filename_out = joinpath(dirname(Base.source_path()), "img/molpng.png")
+    global filename_out =
+        joinpath(dirname(Base.source_path()), "img/molpng.png")
 
     # Delete image file to avoid problems (linux)
-    imgpath = joinpath(dirname(Base.source_path()),"img")
+    imgpath = joinpath(dirname(Base.source_path()), "img")
 
     try
         mkdir(imgpath)
@@ -44,17 +49,13 @@ if Sys.islinux()
     end
 end
 
-################################################################################
 # Load default database
-################################################################################
-@async global databaseDIPPR = CSV.read(pathPUREDIPPR)
+#@async global databaseDIPPR = CSV.read(pathPUREDIPPR)
 
 # Main function
 function OSPropEGUI()
     # Suppress warnings
     #@suppress begin
-    global filename_in
-    global filename_out
 
     # Environmental variable to allow Windows decorations
     ENV["GTK_CSD"] = 0
@@ -74,8 +75,8 @@ function OSPropEGUI()
     set_gtk_property!(winOSPropE, :window_position, 3)
     set_gtk_property!(winOSPropE, :accept_focus, true)
     set_gtk_property!(winOSPropE, :resizable, false)
-    set_gtk_property!(winOSPropE, :width_request, w*0.50)
-    set_gtk_property!(winOSPropE, :height_request, h*0.75)
+    set_gtk_property!(winOSPropE, :width_request, w * 0.50)
+    set_gtk_property!(winOSPropE, :height_request, h * 0.75)
     set_gtk_property!(winOSPropE, :visible, false)
 
     ################################################################################
@@ -84,27 +85,25 @@ function OSPropEGUI()
     # Menu Icons
     tb1 = ToolButton("gtk-new")
     set_gtk_property!(tb1, :label, "New")
-    set_gtk_property!(tb1, :tooltip_markup, "Create a new simulation environment")
+    set_gtk_property!(tb1, :tooltip_markup, "New analysis")
+    signal_connect(tb1, :clicked) do widget
+        empty!(imgSVG)
+        set_gtk_property!(smilesEntry, :text, "")
+        set_gtk_property!(tb6, :sensitive, false)
 
-    tb2 = ToolButton("gtk-open")
-    set_gtk_property!(tb2, :label, "Open")
-    set_gtk_property!(tb2, :tooltip_markup, "Open a simulation file")
+    end
 
-    tb3 = ToolButton("gtk-floppy")
-    set_gtk_property!(tb3, :label, "Export")
-    set_gtk_property!(
-        tb3,
-        :tooltip_markup,
-        "Save the current state environment to a JLD file",
-    )
-    set_gtk_property!(tb3, :sensitive, false)
+    tb2 = ToolButton("gtk-floppy")
+    set_gtk_property!(tb2, :label, "Export")
+    set_gtk_property!(tb2, :tooltip_markup, "Export to .pdf")
+    set_gtk_property!(tb2, :sensitive, false)
 
     # Close toolbar
-    tb4 = ToolButton("gtk-close")
-    set_gtk_property!(tb4, :label, "Close")
-    set_gtk_property!(tb4, :tooltip_markup, "Close current simulation environment")
+    tb3 = ToolButton("gtk-close")
+    set_gtk_property!(tb3, :label, "Close")
+    set_gtk_property!(tb3, :tooltip_markup, "Close")
 
-    signal_connect(tb4, :clicked) do widget
+    signal_connect(tb3, :clicked) do widget
         destroy(winOSPropE)
     end
 
@@ -114,24 +113,33 @@ function OSPropEGUI()
         end
     end
 
-    tb5 = ToolButton("gtk-preferences")
-    set_gtk_property!(tb5, :label, "Tools")
-    set_gtk_property!(tb5, :tooltip_markup, "Tools")
+    tb4 = ToolButton("gtk-preferences")
+    set_gtk_property!(tb4, :label, "Tools")
+    set_gtk_property!(tb4, :tooltip_markup, "Tools")
 
-    tb6 = ToolButton("gtk-about")
-    set_gtk_property!(tb6, :label, "Help")
-    set_gtk_property!(tb6, :tooltip_markup, "Help")
+    tb5 = ToolButton("gtk-about")
+    set_gtk_property!(tb5, :label, "Help")
+    set_gtk_property!(tb5, :tooltip_markup, "Help")
+
+    tb6 = ToolButton("gtk-media-play")
+    set_gtk_property!(tb6, :label, "Run")
+    set_gtk_property!(tb6, :tooltip_markup, "Compute properties")
+    set_gtk_property!(tb6, :sensitive, false)
+    signal_connect(tb6, :clicked) do widget
+        set_gtk_property!(nb, :page, 1)
+    end
+
 
     # Toolbar
     newToolbar = Toolbar()
-    set_gtk_property!(newToolbar, :height_request, (h*.75)*.09)
+    set_gtk_property!(newToolbar, :height_request, (h * 0.75) * 0.09)
     set_gtk_property!(newToolbar, :toolbar_style, 2)
     push!(newToolbar, tb1)
     push!(newToolbar, tb2)
     push!(newToolbar, tb3)
+    push!(newToolbar, tb4)
     push!(newToolbar, tb5)
     push!(newToolbar, tb6)
-    push!(newToolbar, tb4)
 
     gridToolbar = Grid()
     set_gtk_property!(gridToolbar, :column_homogeneous, true)
@@ -147,11 +155,18 @@ function OSPropEGUI()
     # newNotebook
     global nb = Notebook()
     set_gtk_property!(nb, :tab_pos, 0)
-    set_gtk_property!(nb, :height_request, h*.75 - (h*.75)*0.09 - (h*.75)*0.07)
+    set_gtk_property!(
+        nb,
+        :height_request,
+        h * 0.75 - (h * 0.75) * 0.09 - (h * 0.75) * 0.07,
+    )
+    set_gtk_property!(nb, :name, "nb")
     screen = Gtk.GAccessor.style_context(nb)
     push!(screen, StyleProvider(provider), 600)
 
-    # Drawing
+    ################################################################################
+    # Drawing Smiles
+    ################################################################################
     nbFrame0 = Frame()
     screen = Gtk.GAccessor.style_context(nbFrame0)
     push!(screen, StyleProvider(provider), 600)
@@ -166,71 +181,162 @@ function OSPropEGUI()
     set_gtk_property!(gDraw, :column_spacing, 20)
     set_gtk_property!(gDraw, :row_spacing, 20)
 
+    labelSmile = Label("Smile: ")
     smilesEntry = Entry()
-    set_gtk_property!(smilesEntry, :width_request, round(w*.22))
+    set_gtk_property!(smilesEntry, :width_request, round(w * 0.22))
 
     imgSVG = Gtk.Image()
 
     runsmiles = Button("Draw")
     set_gtk_property!(runsmiles, :name, "runsmiles")
-    set_gtk_property!(runsmiles, :width_request, round(w*.08))
-    screen = Gtk.GAccessor.style_context(runsmiles)
-    push!(screen, StyleProvider(provider), 600)
+    set_gtk_property!(runsmiles, :width_request, round(w * 0.08))
 
     signal_connect(runsmiles, :clicked) do widget
-        global filename_in
-        global filename_out
+        global nb
+        try
+            smilesString = get_gtk_property(smilesEntry, :text, String)
 
-        smilesString = get_gtk_property(smilesEntry, :text, String)
+            # Convert String to mol
+            mol = MG.smilestomol(smilesString)
 
-        # Convert String to mol
-        mol = MG.smilestomol(smilesString)
+            # Convert mol to a string of svg format
+            mol_svg =
+                MG.drawsvg(mol, Int(round(h * 0.40)), Int(round(h * 0.40)))
 
-        # Convert mol to a string of svg format
-        mol_svg = MG.drawsvg(mol, Int(round(h*.45)), Int(round(h*.45)))
+            # Export svg file
+            open(filename_in, "w") do io
+                write(io, mol_svg)
+            end
 
-        # Export svg file
-        open(filename_in, "w") do io
-            write(io, mol_svg)
+            # Code needed to convert SVG to PNG
+            r = Rsvg.handle_new_from_file(filename_in)
+            d = Rsvg.handle_get_dimensions(r)
+            cs = Cairo.CairoImageSurface(d.width, d.height, Cairo.FORMAT_ARGB32)
+            c = Cairo.CairoContext(cs)
+            Rsvg.handle_render_cairo(c, r)
+            Cairo.write_to_png(cs, filename_out)
+
+            # Assing png file to Gtk.Image
+            set_gtk_property!(imgSVG, :file, filename_out)
+            Gtk.showall(winOSPropE)
+
+            set_gtk_property!(tb6, :sensitive, true)
+            global status = 1
+        catch
+            Nothing
         end
-
-        # Code needed to convert SVG to PNG
-        r = Rsvg.handle_new_from_file(filename_in)
-        d = Rsvg.handle_get_dimensions(r)
-        cs = Cairo.CairoImageSurface(d.width,d.height,Cairo.FORMAT_ARGB32)
-        c = Cairo.CairoContext(cs)
-        Rsvg.handle_render_cairo(c,r)
-        Cairo.write_to_png(cs,filename_out)
-
-        # Assing png file to Gtk.Image
-        set_gtk_property!(imgSVG, :file, filename_out)
-
-        Gtk.showall(winOSPropE)
     end
 
+    ################################################################################
+    # Buttons for smiles
+    ################################################################################
+    gDrawB = Grid()
+    set_gtk_property!(gDrawB, :valign, 3)
+    set_gtk_property!(gDrawB, :halign, 3)
+    set_gtk_property!(gDrawB, :column_spacing, 20)
+    set_gtk_property!(gDrawB, :row_spacing, 20)
+    set_gtk_property!(gDrawB, :column_homogeneous, true)
+    set_gtk_property!(gDrawB, :row_homogeneous, true)
+
+    doublebond = Button("=")
+    set_gtk_property!(doublebond, :width_request, round(w * 0.078))
+    signal_connect(doublebond, :clicked) do widget
+        smiletext = get_gtk_property(smilesEntry, :text, String)
+        set_gtk_property!(smilesEntry, :text, string(smiletext,"="))
+    end
+
+    triplebond = Button("≡")
+    signal_connect(triplebond, :clicked) do widget
+        smiletext = get_gtk_property(smilesEntry, :text, String)
+        set_gtk_property!(smilesEntry, :text, string(smiletext,"#"))
+    end
+
+    alkane = Button("C-C")
+    alkene = Button("C=C")
+    alkine = Button("C≡C")
+    ring = Button("O")
+    signal_connect(ring, :clicked) do widget
+        smiletext = get_gtk_property(smilesEntry, :text, String)
+        set_gtk_property!(smilesEntry, :text, string(smiletext,"C1CCCCC1"))
+    end
+
+    clear = Button("Clear")
+    signal_connect(clear, :clicked) do widget
+        empty!(imgSVG)
+        set_gtk_property!(smilesEntry, :text, "")
+        set_gtk_property!(tb6, :sensitive, false)
+    end
+
+    expFig = Button("Export")
+
     smilesFrame = Frame()
-    set_gtk_property!(smilesFrame, :name, "smileFrame")
-    set_gtk_property!(smilesFrame, :height_request, round(h*.45))
+    set_gtk_property!(smilesFrame, :height_request, round(h * 0.40))
     screen = Gtk.GAccessor.style_context(smilesFrame)
     push!(screen, StyleProvider(provider), 600)
 
     push!(smilesFrame, imgSVG)
 
-    gDraw[1, 1] = smilesEntry
-    gDraw[2, 1] = runsmiles
-    gDraw[1:2, 2] = smilesFrame
+    gDrawB[1, 1] = doublebond
+    gDrawB[2, 1] = triplebond
+    gDrawB[3, 1] = alkane
+    gDrawB[4, 1] = clear
+
+    gDrawB[1, 2] = alkene
+    gDrawB[2, 2] = alkine
+    gDrawB[3, 2] = ring
+    gDrawB[4, 2] = expFig
+
+    gDraw[1, 1] = labelSmile
+    gDraw[2, 1] = smilesEntry
+    gDraw[3, 1] = runsmiles
+    gDraw[1:3, 2] = smilesFrame
+    gDraw[1:3, 3] = gDrawB
 
 
     push!(nbFrame0, gDraw)
     push!(nb, nbFrame0, "  Drawing  ")
 
+    ################################################################################
     # Resuls
+    ################################################################################
     nbFrame1 = Frame()
     screen = Gtk.GAccessor.style_context(nbFrame1)
     push!(screen, StyleProvider(provider), 600)
+
+    # Notebook for nbResults
+    global nbRes = Notebook()
+    set_gtk_property!(nbRes, :tab_pos, 3)
+    set_gtk_property!(nbRes, :name, "nbRes")
+    screen = Gtk.GAccessor.style_context(nbRes)
+    push!(screen, StyleProvider(provider), 600)
+    #set_gtk_property!(nbRes, :height_request, h*.75 - (h*.75)*0.09 - (h*.75)*0.07)
+
+    nbResFrame0 = Frame()
+    screen = Gtk.GAccessor.style_context(nbResFrame0)
+    push!(screen, StyleProvider(provider), 600)
+
+    nbResFrame1 = Frame()
+    screen = Gtk.GAccessor.style_context(nbResFrame1)
+    push!(screen, StyleProvider(provider), 600)
+
+    nbResFrame2 = Frame()
+    screen = Gtk.GAccessor.style_context(nbResFrame2)
+    push!(screen, StyleProvider(provider), 600)
+
+    push!(nbRes, nbResFrame0, "  Summary  ")
+    push!(nbRes, nbResFrame1, "  Marrero & Gani  ")
+    push!(nbRes, nbResFrame2, "  Jimenez & Sánchez  ")
+
+    push!(nbFrame1, nbRes)
     push!(nb, nbFrame1, "  Results  ")
 
-    gridToolbar[1, 2] = nb
+    # Frame for main notebook
+    nbFrame = Frame()
+    screen = Gtk.GAccessor.style_context(nbFrame)
+    push!(screen, StyleProvider(provider), 600)
+
+    push!(nbFrame, nb)
+    gridToolbar[1, 2] = nbFrame
 
     ################################################################################
     # Acknowledgment
@@ -242,7 +348,7 @@ function OSPropEGUI()
     #set_gtk_property!(gLabel, :margin_bottom, 20)
     set_gtk_property!(gLabel, :margin_left, 20)
     set_gtk_property!(gLabel, :margin_right, 20)
-    set_gtk_property!(gLabel, :height_request, (h*.75)*.07)
+    set_gtk_property!(gLabel, :height_request, (h * 0.75) * 0.07)
 
     newLabel = Label("Designed at Instituto Tecnológico de Celaya/México - 2020
     Support granted by the Conacyt-Secretaría de Energía-Hidrocarburos sectorial fund")
